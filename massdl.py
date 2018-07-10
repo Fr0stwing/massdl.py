9f209eeb29a5b17c66c606b1bcfda5b6d1d1ff80
Fr0stwing <-@-.->
2018-07-10 05:46:05 -0400

    Added variables to make it a bit clearer

--- massdl.py
+++ massdl.py
@@ -1,13 +1,15 @@
 import urllib.request
 
-i = 1
-while i < 10:
+startNum = 1
+stopNum = 10
+
+while startNum < stopNum:
 	try:
 		# First arg: file location / Second arg: file name (with extension)
-		urllib.request.urlretrieve("http://*.*/.gif?id="+str(i), str(i)+".gif")
+		urllib.request.urlretrieve("http://*.*/.gif?id="+str(startNum), str(startNum)+".gif")
 	except Exception:
-		print("File " + str(i) + " doesn't exist. - Moving on...")
-		i += 1
+		print("File " + str(startNum) + " doesn't exist. - Moving on...")
+		startNum += 1
 	else:
-		print("Saved file number " + str(i))
-		i += 1
+		print("Saved file number " + str(startNum))
+		startNum += 1
