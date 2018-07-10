94d08e2b4a7d6da48af7bad73fa5deb8f28f44df
Fr0stwing <-@-.->
2018-07-10 05:29:04 -0400

    Added the basics

---
+++ massdl.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+i = 1
+while i < 10:
+	try:
+		# First arg: file location / Second arg: file name (with extension)
+		urllib.request.urlretrieve("http://*.*/.gif?id="+str(i), str(i)+".gif")
+	except Exception:
+		print("File " + str(i) + " doesn't exist. - Moving on...")
+		i += 1
+	else:
+		print("Saved file " + str(i) + ".gif")
+		i += 1
